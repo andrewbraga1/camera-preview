c7e9dd372e9c9f18495f9b81510948f373a95ed3
Laboratorio Inovacao <alunom13@acad.unifor.br>
2019-03-28 17:30:18 -0300

    Building a better camera flow

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -4,7 +4,7 @@ import { CameraPreview, CameraPreviewPictureOptions, CameraPreviewOptions, Camer
 import { Platform } from 'ionic-angular';
 import { Component,ElementRef,ViewChild } from '@angular/core';
 import { ImagepagePage } from '../imagepage/imagepage';
-
+import { ToastController } from 'ionic-angular';
  // 'plug into' DOM canvas element using @ViewChild
 
 @Component({
@@ -21,27 +21,21 @@ private _CANVAS  : any;
 private _CONTEXT : any;
 picture:any="";
 cameraState:Boolean = false;
-  constructor(platform: Platform,public navCtrl: NavController,private cameraPreview: CameraPreview) {
-    /* platform.ready().then(() => {
-      
-      // Okay, so the platform is ready and our plugins are available.
-      // Here you can do any higher level native things you might need.
-     
-    }); */
-    
+confirm_photo:Boolean =false;
+
+  constructor(platform: Platform,public navCtrl: NavController,private cameraPreview: CameraPreview,public toastCtrl: ToastController) {
+       
   }
 
   ionViewDidLoad(){
     this._CANVAS 	    = this.canvasEl.nativeElement;
     this._CANVAS.width  	= window.innerWidth     
     this._CANVAS.height 	= window.innerHeight  
-
+    
     this.initialiseCanvas();
     
-    
   }
-
-
+  
   initialiseCanvas(){
     if(this._CANVAS.getContext)
     {
@@ -51,13 +45,24 @@ cameraState:Boolean = false;
 
   setupCanvas(){
     this._CONTEXT = this._CANVAS.getContext('2d');
-    //this._CONTEXT.fillStyle = "#3e3e3e";
-    //this._CONTEXT.fillRect(0, 50, 500,500);//0,0,500,500
   }
+
+  drawCircle(){
+    
+    this._CONTEXT.beginPath();
+
+    // x, y, radius, startAngle, endAngle
+    this._CONTEXT.arc(this._CANVAS.width/2, this._CANVAS.height/2, 150, 0, 2 * Math.PI);
+    this._CONTEXT.lineWidth = 6;
+    this._CONTEXT.strokeStyle = '#0000FF'; /* '#ffffff' */
+    this._CONTEXT.stroke();
+    
+      
+}
 
   // camera options (Size and location). In the following example, the preview uses the rear camera and display the preview in the back of the webview
 initCamera(){
-  const cameraPreviewOpts: CameraPreviewOptions = {
+    const cameraPreviewOpts: CameraPreviewOptions = {
     x: 0,
     y: 0,
     width: window.screen.width,
@@ -73,20 +78,16 @@ initCamera(){
 
   this.cameraPreview.startCamera(cameraPreviewOpts).then(
     (res) => {
-      this.cameraState = true
-      if(this.cameraState==true){
-        this.drawCircle();
-      }
       
-      //alert(res)
+      this.cameraState = true
+      this.drawCircle();
+      
     },
     (err) => {
       console.log(err)
-      //alert("start: "+err)
+      
     }); 
    
-
-  
 }  
 
  takePicture(){
@@ -102,16 +103,43 @@ initCamera(){
   // take a picture
   this.cameraPreview.takePicture(pictureOpts).then((imageData) => {
     this.picture = 'data:image/jpeg;base64,' + imageData;
-    //this.cameraPreview.hide();
-    this._CANVAS.style.display="none";
+    this.cameraPreview.hide();
+    this._CANVAS.style.visibility="hidden";
+    //this._CANVAS.style.display="none";
+    //this._CANVAS.style.opacity=0;
     this.content.setElementStyle("background-color","white")
+    this.cameraState = false
   }, (err) => {
     console.log(err);
    
    
   });
+  this.cameraState = false
 }  
-
+cancel(){
+  this.reset_camera_preview()
+  
+}
+use_photo(){
+  this.confirm_photo = true
+  this.picture=""
+  const toast = this.toastCtrl.create({
+    message: 'Photo was added successfully',
+    duration: 3000
+  });
+  toast.present();
+ this.reset_camera_preview()
+  
+}
+reset_camera_preview(){
+  this.picture=""
+  this.cameraPreview.show()
+  this._CANVAS.style.visibility="visible";
+  //this._CANVAS.style.display="block";
+  //this._CANVAS.style.opacity=1;
+  this.content.setElementStyle("background-color","white")
+  this.cameraState= true
+}
 cameraSwitch(){  
   // Switch camera
   this.cameraPreview.switchCamera();
@@ -126,15 +154,6 @@ terminateCamera(){
   this.cameraPreview.hide();
 }
  
- drawCircle(){
-      
-      this._CONTEXT.beginPath();
-
-      // x, y, radius, startAngle, endAngle
-      this._CONTEXT.arc(this._CANVAS.width/2, this._CANVAS.height/2, 150, 0, 2 * Math.PI);
-      this._CONTEXT.lineWidth = 10;
-      this._CONTEXT.strokeStyle = '#0000FF'; /* '#ffffff' */
-      this._CONTEXT.stroke();
-  }
+  
 }
  
